66550586147f20514da764598062beb82a863665
rajan5707 <rajan5707>
2022-08-29 18:43:36 +0000

    change of port to 9000

--- createCont.sh
+++ createCont.sh
@@ -5,9 +5,9 @@ if [ $? -ne 0 ] ; then
 #        exit 1
 fi
 
-sudo docker run -d -it --name angcontain -p 9999:80 angularv1:av1
+sudo docker run -d -it --name angcontain -p 9000:80 angularv1:av1
 if [ $? -ne 0 ] ; then
         echo "ERROR: Docker build failed"
         exit 1
 fi
-echo "====CONTAINER IS RUNNING AT PORT = 9092"
+echo "====CONTAINER IS RUNNING AT PORT = 9000"

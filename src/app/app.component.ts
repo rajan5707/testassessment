05cb411e8476f5c7029acefae049b28a5a6a875e
rajan5707 <rajanraj5707@gmail.com>
2022-08-29 05:33:05 +0000

    final changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,10 @@ totalAngularPackages:any;
 		var routeUrl = window.location.href
 		var url = routeUrl.slice(0,-5)
 		console.log("launching url > ",url)
-			this.http.get("http://13.232.124.37:5000/write").subscribe(data => {
+			this.http.get(url +":5000/write").subscribe(data => {
 			console.log(" written data",data)
      			})
-			this.http.get("http://13.232.124.37:5000/read").subscribe(data => {
+			this.http.get(url+":5000/read").subscribe(data => {
 			console.log("read data >> ",data)
    			})
    		}
